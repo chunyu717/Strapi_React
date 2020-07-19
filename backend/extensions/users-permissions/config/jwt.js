0a27afa234d104ec1e8673b948526116853b7c2e
kingbike <kc109763@gmail.com>
2020-07-19 17:17:18 +0800

    first commit

---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e0eef80d-94d9-44ad-b32d-bd6eaf091328'
+};
---
+++ frontend/src/containers/App/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Articles from "../Articles";
+import Nav from "../../components/Nav";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Articles />
+    </div>
+  );
+}
+
+export default App;
